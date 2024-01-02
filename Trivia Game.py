ed7287816012e64f145ec226bda2e16dc9b8136d
sami.imamoglu <ssimamoglu@gmail.com>
2024-01-02 11:30:44 +0300

    commit

--- Trivia Game.py
+++ Trivia Game.py
@@ -1,4 +1,7 @@
 
+import random
+
+
 class Question():
     triviaquestion = ""
     def __init__(self, triviaquestion, answer1, answer2, answer3, answer4, correctanswer):
@@ -8,31 +11,66 @@ class Question():
     def getAnswer(self, index):
         return self.answers[index]
         
-    
-# trquestions = Question[10]
-# trquestions[0] = Question("Bir bir daha kaç eder?", "1", "2", "3", "4", 2)
-# trquestions[1] = Question("iki kere iki  kaç eder?", "1", "2", "3", "4", )
-# trquestions[2] = Question("Bir bir daha kaç eder?", "1", "2", "3", "4", 2)
-# trquestions[3] = Question("Bir bir daha kaç eder?", "1", "2", "3", "4", 2)
-# trquestions[4] = Question("Bir bir daha kaç eder?", "1", "2", "3", "4", 2)
-# trquestions[5] = Question("Bir bir daha kaç eder?", "1", "2", "3", "4", 2)
-# trquestions[6] = Question("Bir bir daha kaç eder?", "1", "2", "3", "4", 2)
-# trquestions[7] = Question("Bir bir daha kaç eder?", "1", "2", "3", "4", 2)
-# trquestions[8] = Question("Bir bir daha kaç eder?", "1", "2", "3", "4", 2)
-# trquestions[9] = Question("Bir bir daha kaç eder?", "1", "2", "3", "4", 2)
+qSet = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+def askQuestion(trquestions):
+        for x in qSet:
+            r = random.randint(1,10)
+            print("x:",x,"r:",r)
+            if (x == r):
+                qSet.remove(r)
+                break
+        q = trquestions[r-1]
+        print(q.triviaquestion)
+        ord = 1
+        for a in q.answers:
+            print(ord, ":" , a)
+            ord = ord + 1
+        return q
+
+def checkAnswer(q, ans):
+        print(q.getAnswer(ans-1))
+        if (ans == q.correctanswer):
+            print("Success")
+            return 1
+        else: 
+            print("False")
+            return 0
+
 
 def main():
-    trquestions = Question[10]
-    trquestions[0] = Question("Bir bir daha kaç eder?", "1", "2", "3", "4", 2)
-    trquestions[1] = Question("iki kere iki  kaç eder?", "1", "2", "3", "4", 4)
-    trquestions[2] = Question("Bir bir daha kaç eder?", "1", "2", "3", "4", 2)
-    trquestions[3] = Question("Bir bir daha kaç eder?", "1", "2", "3", "4", 2)
-    trquestions[4] = Question("Bir bir daha kaç eder?", "1", "2", "3", "4", 2)
-    trquestions[5] = Question("Bir bir daha kaç eder?", "1", "2", "3", "4", 2)
-    trquestions[6] = Question("Bir bir daha kaç eder?", "1", "2", "3", "4", 2)
-    trquestions[7] = Question("Bir bir daha kaç eder?", "1", "2", "3", "4", 2)
-    trquestions[8] = Question("Bir bir daha kaç eder?", "1", "2", "3", "4", 2)
-    trquestions[9] = Question("Bir bir daha kaç eder?", "1", "2", "3", "4", 2)
+    trquestions = []
+    trquestions.append(Question("Bir bir daha kaç eder?", "1", "2", "3", "4", 2))
+    trquestions.append(Question("iki kere iki  kaç eder?", "1", "2", "3", "4", 4))
+    trquestions.append(Question("üç üç daha kaç eder?", "6", "2", "3", "4", 1))
+    trquestions.append(Question("dört dört daha kaç eder?", "12", "16", "8", "2", 3))
+    trquestions.append(Question("beş beş daha kaç eder?", "10", "20", "30", "40", 1))
+    trquestions.append(Question("altı altı daha kaç eder?", "11", "12", "13", "14", 2))
+    trquestions.append(Question("yedi yedi daha kaç eder?", "15", "21", "31", "14", 4))
+    trquestions.append(Question("sekiz sekiz daha kaç eder?", "16", "24", "32", "40", 1))
+    trquestions.append(Question("dokuz dokuz daha kaç eder?", "18", "22", "32", "49", 1))
+    trquestions.append(Question("on kere on kaç eder?", "10", "20", "30", "100", 4))
 
-    for q in trquestions:
-        print(q)
+    playerOneScore = 0
+    playerTwoScore = 0
+
+    for qc in range(1,5):
+        print("Player1 it's your turn")
+        q = askQuestion(trquestions)
+        ans = int(input("What's your answer? "))
+        playerOneScore = playerOneScore + checkAnswer(q, ans)
+        print("Player2 it's your turn")
+        q = askQuestion(trquestions)
+        ans = int(input("What's your answer? "))
+        playerTwoScore = playerTwoScore + checkAnswer(q, ans)
+        print("Player1: ", playerOneScore, " Player2: ", playerTwoScore)
+
+    print(playerOneScore, playerTwoScore)
+    if (playerOneScore > playerTwoScore):
+        print("Player1 is the winner")
+    elif (playerOneScore < playerTwoScore):
+        print("Player2 is the winner")
+    elif (playerOneScore == playerTwoScore):
+        print("Drawback")
+
+main()
